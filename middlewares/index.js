61569e57d79f6ab3ef692aec50669ad3d64cf84b
Cesar <Cesar24gas@gmail.com>
2021-03-18 00:58:16 +0100

    Actualizacion de rutas

---
+++ middlewares/index.js
@@ -0,0 +1,12 @@
+
+
+const validaJWT = require('../middlewares/validar-jwt');
+const validaCampos = require('../middlewares/validar-campos');
+const validaRoles = require('../middlewares/validar-roles');
+
+module.exports = {
+
+    ...validaCampos,
+    ...validaJWT,
+    ...validaRoles,
+}
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,15 +2,21 @@ const {Router} = require('express');
 const {check} = require('express-validator');
 const Role = require('../models/role')
 
+const {
+    validarCampos,
+    validarJWT,
+     tieneRole,
+    esAdminRole
+} = require('../middlewares')
+
 const { usuarioGet,
         usuarioDelete, 
         usuarioPatch,
         usuarioPost, 
         usuarioPut} = require('../controllers/usuarios');
-const { validarCampos } = require('../middlewares/validar-campos');
+
 const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
-const { validarJWT } = require('../middlewares/validar-jwt');
-const { esAdminRole, tieneRole } = require('../middlewares/validar-roles');
+
 
 const router = Router();
 
